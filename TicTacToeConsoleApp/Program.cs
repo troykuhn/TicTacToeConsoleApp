87f0294d8b9d4fdff6e39e94a43c8ff68c7d6068
Troy Kuhn <troykuhn22@gmail.com>
2020-05-28 13:43:32 -0400

    Finsihed product

--- TicTacToeConsoleApp/Program.cs
+++ TicTacToeConsoleApp/Program.cs
@@ -10,12 +10,21 @@ namespace TicTacToeConsoleApp
 	{
 
 		//the field of play
-		static char[,] playfield =
+		static char[,] playField =
 		{
 			{'1', '2', '3' },
 			{'4', '5', '6' },
 			{'7', '8', '9' }
 		};
+
+		static char[,] playFieldInitial =
+		{
+			{'1', '2', '3' },
+			{'4', '5', '6' },
+			{'7', '8', '9' }
+		};
+
+		static int turns = 0;
 
 		static void Main( string[] args )
 		{
@@ -29,19 +38,61 @@ namespace TicTacToeConsoleApp
 				if ( player == 2 )
 				{
 					player = 1;
-					EnterXorO( player, input );
+					EnterXorO( 'O', input );
 				}
 				else if ( player == 1 )
 				{
 					player = 2;
-					EnterXorO( player, input );
+					EnterXorO( 'X', input );
 				}
 
 				SetField();
+				#region
+				//Check Winning condition
+				char[] playerChars = { 'X', 'O' };
 
+				foreach ( char playerChar in playerChars )
+				{
+					if ( ( ( playField[ 0, 0 ] == playerChar ) && ( playField[ 0, 1 ] == playerChar ) && ( playField[ 0, 2 ] == playerChar ) )
+						|| ( ( playField[ 1, 0 ] == playerChar ) && ( playField[ 1, 1 ] == playerChar ) && ( playField[ 1, 2 ] == playerChar ) )
+						|| ( ( playField[ 2, 0 ] == playerChar ) && ( playField[ 2, 1 ] == playerChar ) && ( playField[ 2, 2 ] == playerChar ) )
+						|| ( ( playField[ 0, 0 ] == playerChar ) && ( playField[ 1, 0 ] == playerChar ) && ( playField[ 2, 0 ] == playerChar ) )
+						|| ( ( playField[ 0, 1 ] == playerChar ) && ( playField[ 1, 1 ] == playerChar ) && ( playField[ 2, 1 ] == playerChar ) )
+						|| ( ( playField[ 0, 2 ] == playerChar ) && ( playField[ 2, 1 ] == playerChar ) && ( playField[ 2, 2 ] == playerChar ) )
+						|| ( ( playField[ 0, 0 ] == playerChar ) && ( playField[ 1, 1 ] == playerChar ) && ( playField[ 2, 2 ] == playerChar ) )
+						|| ( ( playField[ 0, 2 ] == playerChar ) && ( playField[ 1, 1 ] == playerChar ) && ( playField[ 2, 0 ] == playerChar ) )
+						)
+					{
+						if ( playerChar == 'X' )
+						{
+							Console.WriteLine( "\n Player 2 has won!" );
+						}
+						if ( playerChar == 'O' )
+						{
+							Console.WriteLine( "\n Player 1 has won!" );
+						}
+
+						Console.WriteLine( "Press any key to reset the game." );
+						Console.ReadKey();
+						ResetField();
+						break;
+					}
+					else if ( turns == 10 )
+					{
+						Console.WriteLine( "\n Game has ended in a DRAW." );
+						Console.WriteLine( "Press any key to reset the game." );
+						Console.ReadKey();
+						ResetField();
+						break;
+					}
+				}
+				#endregion
+
+				#region
+				//Tests if field has been taken or not.
 				do
 				{
-					Console.Write( "\nPlayer {0}: Choose your field! ", player);
+					Console.Write( "\nPlayer {0}: Choose your field! ", player );
 					try
 					{
 						input = Convert.ToInt32( Console.ReadLine() );
@@ -49,66 +100,83 @@ namespace TicTacToeConsoleApp
 					catch
 					{
 
-						Console.WriteLine("Please enter a number");
+						Console.WriteLine( "Please enter a number" );
 					}
 
-					if ( (input == 1) && ( playfield[ 0, 0 ] == '1' ) )
+					if ( ( input == 1 ) && ( playField[ 0, 0 ] == '1' ) )
 						validInput = true;
-					else if ( ( input == 2 ) && ( playfield[ 0, 1 ] == '2' ) )
+					else if ( ( input == 2 ) && ( playField[ 0, 1 ] == '2' ) )
 						validInput = true;
-					else if ( ( input == 3 ) && ( playfield[ 0, 2 ] == '3' ) )
+					else if ( ( input == 3 ) && ( playField[ 0, 2 ] == '3' ) )
 						validInput = true;
-					else if ( ( input == 4 ) && ( playfield[ 1, 0 ] == '4' ) )
+					else if ( ( input == 4 ) && ( playField[ 1, 0 ] == '4' ) )
 						validInput = true;
-					else if ( ( input == 5 ) && ( playfield[ 1, 1 ] == '5' ) )
+					else if ( ( input == 5 ) && ( playField[ 1, 1 ] == '5' ) )
 						validInput = true;
-					else if ( ( input == 6 ) && ( playfield[ 1, 2 ] == '6' ) )
+					else if ( ( input == 6 ) && ( playField[ 1, 2 ] == '6' ) )
 						validInput = true;
-					else if ( ( input == 7 ) && ( playfield[ 2, 0 ] == '7' ) )
+					else if ( ( input == 7 ) && ( playField[ 2, 0 ] == '7' ) )
 						validInput = true;
-					else if ( ( input == 8 ) && ( playfield[ 2, 1 ] == '8' ) )
+					else if ( ( input == 8 ) && ( playField[ 2, 1 ] == '8' ) )
 						validInput = true;
-					else if ( ( input == 9 ) && ( playfield[ 2, 2 ] == '9' ) )
+					else if ( ( input == 9 ) && ( playField[ 2, 2 ] == '9' ) )
 						validInput = true;
+					else
+					{
+						Console.WriteLine( "\nField has already been selected. Please select another field." );
+						validInput = false;
+					}
 
 				} while ( !validInput );
+
+				#endregion
 			}
 			while ( true );
+		}
+
+		public static void ResetField()
+		{
+			char[,] playFieldInitial =
+			{
+				{'1', '2', '3' },
+				{'4', '5', '6' },
+				{'7', '8', '9' }
+			};
+
+			playField = playFieldInitial;
+			SetField();
+			turns = 1;
 		}
 
 		public static void SetField()
 		{
 			Console.Clear();
 			Console.WriteLine( "     |     |     " );
-			Console.WriteLine( "  {0}  |  {1}  |  {2}", playfield[ 0, 0 ], playfield[ 0, 1 ], playfield[ 0, 2 ] );
+			Console.WriteLine( "  {0}  |  {1}  |  {2}", playField[ 0, 0 ], playField[ 0, 1 ], playField[ 0, 2 ] );
 			Console.WriteLine( "_____|_____|_____" );
 			Console.WriteLine( "     |     |     " );
-			Console.WriteLine( "  {0}  |  {1}  |  {2}", playfield[ 1, 0 ], playfield[ 1, 1 ], playfield[ 1, 2 ] );
+			Console.WriteLine( "  {0}  |  {1}  |  {2}", playField[ 1, 0 ], playField[ 1, 1 ], playField[ 1, 2 ] );
 			Console.WriteLine( "_____|_____|_____" );
 			Console.WriteLine( "     |     |     " );
-			Console.WriteLine( "  {0}  |  {1}  |  {2}", playfield[ 2, 0 ], playfield[ 2, 1 ], playfield[ 2, 2 ] );
+			Console.WriteLine( "  {0}  |  {1}  |  {2}", playField[ 2, 0 ], playField[ 2, 1 ], playField[ 2, 2 ] );
 			Console.WriteLine( "     |     |     " );
+			turns++;
 		}
 
-		public static void EnterXorO( int player, int input )
+		public static void EnterXorO( char playerSign, int input )
 		{
-			char playerSign = ' ';
-			if ( player == 1 )
-				playerSign = 'X';
-			else if ( player == 2 )
-				playerSign = 'O';
 
 			switch ( input )
 			{
-				case 1: playfield[ 0, 0 ] = playerSign; break;
-				case 2: playfield[ 0, 1 ] = playerSign; break;
-				case 3: playfield[ 0, 2 ] = playerSign; break;
-				case 4: playfield[ 1, 0 ] = playerSign; break;
-				case 5: playfield[ 1, 1 ] = playerSign; break;
-				case 6: playfield[ 1, 2 ] = playerSign; break;
-				case 7: playfield[ 2, 0 ] = playerSign; break;
-				case 8: playfield[ 2, 1 ] = playerSign; break;
-				case 9: playfield[ 2, 2 ] = playerSign; break;
+				case 1: playField[ 0, 0 ] = playerSign; break;
+				case 2: playField[ 0, 1 ] = playerSign; break;
+				case 3: playField[ 0, 2 ] = playerSign; break;
+				case 4: playField[ 1, 0 ] = playerSign; break;
+				case 5: playField[ 1, 1 ] = playerSign; break;
+				case 6: playField[ 1, 2 ] = playerSign; break;
+				case 7: playField[ 2, 0 ] = playerSign; break;
+				case 8: playField[ 2, 1 ] = playerSign; break;
+				case 9: playField[ 2, 2 ] = playerSign; break;
 			}
 		}
 	}
